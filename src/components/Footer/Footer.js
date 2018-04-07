250997fd4a6e9b4e4555bd97cd715a8277e5f5c4
Mahmoud Abdulazim <mahmoud.abdulazim@robustastudio.com>
2018-04-08 01:43:43 +0200

    Initial commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Footer = () => (
+	<footer>
+		<p>Copyrights &copy; 2018</p>
+	</footer>
+);
+
+export default Footer;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Header = () => (
+	<header>
+		<h1 className="logo">
+			<a href="">Headless WP</a>
+		</h1>
+		<nav className="navbar">
+			<ul className="navbar__list">
+				<li className="navbar__item">
+					<a href="" className="navbar__link">Home</a>
+				</li>
+				<li className="navbar__item">
+					<a href="" className="navbar__link">Posts</a>
+				</li>
+			</ul>
+		</nav>
+	</header>
+);
+
+export default Header;
